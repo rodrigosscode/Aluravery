9de9921533a77b40bc8989f758471cec2b71d07e
rodrigosantos <rodrigosilvasantos2@gmail.com>
2022-09-03 20:36:48 -0300

    [Alura]#3 - Doing Challenge

--- app/src/main/java/br/com/sscode/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/sscode/aluvery/MainActivity.kt
@@ -5,10 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.sscode.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,11 +32,40 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
+fun Challenge() = Row(
+    modifier = Modifier
+        .height(80.dp)
+        .width(200.dp)
+) {
+    Box(
+        modifier = Modifier
+            .background(color = Color.Blue)
+            .fillMaxHeight()
+            .fillMaxWidth(0.3f)
+    )
+    Column {
+        Text(
+            text = "Test 1", modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.LightGray)
+                .padding(10.dp),
+            fontSize = 6.sp
+        )
+        Text(text = "Test 2", modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+            fontSize = 6.sp)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
 fun ProductItem() = Column {
-    Box(modifier = Modifier
-        .height(50.dp)
-        .background(color = Color.Blue)
-        .width(50.dp)
+    Box(
+        modifier = Modifier
+            .height(50.dp)
+            .background(color = Color.Blue)
+            .width(50.dp)
     )
     Image(
         painter = painterResource(id = R.drawable.ic_launcher_background),
