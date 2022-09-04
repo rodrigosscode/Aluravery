c745ffec8db6895fdcfcaf86fd9df8e90cac2f2d
rodrigosantos <rodrigosilvasantos2@gmail.com>
2022-09-04 16:24:54 -0300

    [Alura]#3 - Align, Clip, Spacer, Size, Offset, MaxMin Height, Brush (gradient), Column Scope to define Modifiers, Shapes (CornerRadius, CircleShape), Util LoremIpsum

--- app/src/main/java/br/com/sscode/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/sscode/aluvery/MainActivity.kt
@@ -6,16 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.sscode.aluvery.ui.theme.AluveryTheme
+import br.com.sscode.aluvery.ui.theme.Purple500
+import br.com.sscode.aluvery.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,17 +41,56 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun ProductItem() = Column {
-    Box(
-        modifier = Modifier
-            .height(50.dp)
-            .background(color = Color.Blue)
-            .width(50.dp)
-    )
-    Image(
-        painter = painterResource(id = R.drawable.ic_launcher_background),
-        contentDescription = "Imagem do produto"
-    )
-    Text(text = "Texto 1")
-    Text(text = "Texto 2")
+fun ProductItem() {
+    Surface(
+        shape = RoundedCornerShape(15.dp),
+        elevation = 4.dp,
+        modifier = Modifier.padding(6.dp)
+    ) {
+        Column(
+            Modifier
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .height(imageSize)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500, Teal200
+                            )
+                        )
+                    )
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Imagem do produto",
+                    modifier = Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize / 2)
+                        .clip(shape = CircleShape)
+                        .align(Center),
+                )
+            }
+            Spacer(modifier = Modifier.height(imageSize / 2))
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "R$ 14.99",
+                    modifier = Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+            }
+        }
+    }
 }
